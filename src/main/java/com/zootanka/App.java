65ec0201a0285f14310964f81d6e55f5a21d9807
Zoltan Martonos <z.martonos@searchmetrics.com>
2020-01-22 00:10:25 +0100

    fixes

--- src/main/java/com/zootanka/App.java
+++ src/main/java/com/zootanka/App.java
@@ -8,11 +8,17 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Stocks World!");
         // tencent
-        int year = 2009;
-        String earnings = "0.5700	 	0.8900	 	1.1200	 	1.3900	 	1.6900 2.5790	 	3.0970	 	4.3830	 	7.5980	 	8.3360  10.95 11.42 	13.87 	15.92 	20.16";
+        String earnings700 = "0.5700	 	0.8900	 	1.1200	 	1.3900	 	1.6900 2.5790	 	3.0970	 	4.3830	 	7.5980	 	8.3360  10.95 11.42 	13.87 	15.92 	20.16";
 
-        final Stock tencent = new Stock("Tencent", "700", "isin....");
-        tencent.setEarnings(year, earnings);
-        System.out.println(tencent.simulatePe(35));
+        System.out.println(Stock.of("Tencent", "HKEX:700", earnings700, 2009).simulatePe(35));
+
+        // alibaba
+        String earningBaba = "$0.04 $0.05 $0.29 $0.57 $1.62 $1.55 $4.29 $2.46 $3.88 $4.97 9.02 11.14 15.5";
+
+        System.out.println(Stock.of("Alibaba", "NYSE:BABA", earningBaba, 2010).simulatePe(29));
+
+        // xilinx
+        String earningXilinx = "1.0100 1.0400 1.2700 1.3600 1.3000 2.4300 2.0100 1.8600 2.3700 2.4400 2.1400 2.4700 2.0500 3.5200 3.52 3.6 4.8";
+        System.out.println(Stock.of("Xilinx", "NASDAQ:XLNX", earningXilinx, 2006).simulatePe(27));
     }
 }
--- src/main/java/com/zootanka/Stock.java
+++ src/main/java/com/zootanka/Stock.java
@@ -13,14 +13,17 @@ public class Stock {
     private List<Float> earnings = new ArrayList<>();
     private int earningsYearStart;
 
-    public Stock(final String name, final String ticker, final String isin) {
-        this.isin = isin;
+    public Stock(final String name, final String ticker) {
+        this.name = name;
+        this.ticker = ticker;
     }
 
     public void setEarnings(final int earningsYearStart, final String text) {
         this.earningsYearStart = earningsYearStart;
         final String[] es = text.split("\\s+");
-        Arrays.stream(es).forEach(v -> earnings.add(Float.parseFloat(v)));
+        Arrays.stream(es)
+                .map(v -> v.replaceAll("[^\\d.]", ""))
+                .forEach(v -> earnings.add(Float.parseFloat(v)));
         System.out.println(earnings);
     }
 
@@ -31,9 +34,15 @@ public class Stock {
         IntStream.range(0, earnings.size()).mapToObj(i -> {
             float stockPrice = pe * earnings.get(i);
             int simYear = earningsYearStart + i;
-            return String.format("%d: %2.2f%s", simYear, stockPrice, simYear > year ? "e\n" : "\n");
+            return String.format("%d: %2.2f%s", simYear, stockPrice, simYear > year-1 ? "e\n" : "\n");
         }).forEach(builder::append);
 
         return builder.toString();
+    }
+
+    public static Stock of(final String name, final String ticker, final String earnings, final int year) {
+        final Stock s = new Stock(name, ticker);
+        s.setEarnings(year, earnings);
+        return s;
     }
 }
